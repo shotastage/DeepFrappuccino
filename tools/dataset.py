3f68c45c78f1bbec07657f6792c4ca3843183aa7
Shota Shimazu <hornet.live.mf@gmail.com>
2018-12-01 12:32:49 +0900

    revise

---
+++ tools/dataset.py
@@ -0,0 +1,14 @@
+import os
+
+
+frappuccino_data = {
+    "white_snow_chocolate_frappuccino": {
+        "title": "White Snow Chocolate Frappuccino",
+        "size": "tall" 
+    },
+}
+
+
+
+def hiec_jpg():
+    pass
